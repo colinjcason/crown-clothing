1aedfb9ee1ce5d038242d37a66f64a9085051345
Colin Cason <colinjcason@gmail.com>
2021-02-25 22:27:42 -0800

    created sign in page and button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/ShopPage'
 import Header from './components/header/Header'
+import SignInPage from './pages/sign-in-page/SignInPage'
 import { Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-page/SignInPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/SignIn'
+
+const SignInPage = () => (
+    <div className='sign-in'>
+        <SignIn />
+    </div>
+)
+
+export default SignInPage
