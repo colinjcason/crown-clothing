5153d142a783db533f198b4184c4dddcf9d38b40
Colin Cason <colinjcason@gmail.com>
2021-03-10 18:04:51 -0800

    added user redirect when not signed in and added redux user login actions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/HomePage'
 import ShopPage from './pages/ShopPage'
 import Header from './components/header/Header'
 import SignInPage from './pages/sign-in-page/SignInPage'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/user.actions.js'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
@@ -40,15 +40,19 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInPage} />
+        <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInPage/>)} />
       </Switch>
     </div>
   );
 }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
