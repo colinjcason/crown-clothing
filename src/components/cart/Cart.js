95065d3c124fc6e0e58cb3ac31924a5dc43f0a98
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2021-03-16 21:31:10 -0700

    added items to cart with redux reducer and update cart quantity

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './cart-dropdown.styles.scss'
 import Button from '../button/Button'
+import CartItem from '../cart-item/CartItem'
+import { connect } from 'react-redux'
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'></div>
+        <div className='cart-items'>
+            {cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))}
+        </div>
         <Button>GO TO CHECKOUT</Button>
     </div>
 )
 
-export default Cart
+const mapStateToProps = ({ cart: {cartItems} }) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(Cart)
