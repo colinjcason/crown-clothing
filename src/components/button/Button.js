03888bbe6cb7d83ada847a147c1b7b0f359fa2b9
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2021-03-16 21:12:10 -0700

    created add item to cart and redux functionality for adding to cart

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './button.styles.scss'
 
-const Button = ({ children, isGoogleSignIn, ...otherProps }) => (
+const Button = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
     <button 
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+    className={`${inverted ? 'inverted' : ''} 
+    ${isGoogleSignIn ? 'google-sign-in' : ''} 
+    custom-button`} 
+    {...otherProps}>
         {children}
     </button>
 )
